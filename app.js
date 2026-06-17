279666903efa3622883304012654ea8edb877ec0
a34846912-spec <a34846912@gmail.com>
2026-06-17 17:01:44 +0900

    Update app.js

--- app.js
+++ app.js
@@ -137,9 +137,10 @@ function today() {
 
 
 function isServiceOpen() {
-
-  return true;
-
+  const now = new Date();
+  const day = now.getDay();
+  const hour = now.getHours();
+  return day >= 1 && day <= 5 && hour >= 9 && hour < 16;
 }
 
 
